a25b4b8a3b2f67f9b71a5c72456baecf747cd82e
Nathanrchrd <80324162+Nathanrchrd@users.noreply.github.com>
2022-04-01 10:54:29 +0200

    interfaceutili

--- eleve/interfaceEleve.php
+++ eleve/interfaceEleve.php
@@ -1,19 +1,84 @@
 <?php
 include ('../fonction.php');
 ?>
+
+<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link rel="stylesheet" href="Styles/styles.css" type="text/css" />
-        <?php
-           session_set_cookie_params(1);
-           session_start();
-        ?>
+        <meta charset="UTF-8">
+        <META HTTP-EQUIV="Refresh" CONTENT="; URL=http://localhost/TP-MITM/briefing.php"> 
+        <link href="../Styles/styles.css" rel="stylesheet" type="text/css">
+        <title> Briefing MITM </title>
     </head>
-    <?php
-        echo idEleve(1);
-        echo loginEleve(1);
-    ?>
-
+    <body>
+        <div class="console_requete">
+           <div class="requete">
+               <br>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <br>
+           </div>
+        </div>
+        <div class="console_reponse">
+            <div class="reponse">
+                <br>
+                <p>qsdsq</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsdsq</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsdsq</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsdsq</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsdsq</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsdsq</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsdsq</p><p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <p>qsd</p>
+                <br>
+            </div>
+        </div>
+    </body>
 </html>
