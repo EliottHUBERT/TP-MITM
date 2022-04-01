fc12aa04b579fb2c829f4f3b99f65c008f2f152c
BOUVIER Gaston <g.bouvier@eleve.leschartreux.net>
2022-04-01 09:24:11 +0200

    refonte style

--- connexion.php
+++ connexion.php
@@ -8,3 +8,12 @@ html>
            session_start();
         ?>
     </head>
+
+    <body>
+        <form method='POST'>
+        De combien d'utilisateur avez-vous besoin ?"."
+                        <input type='number' name='nombreutilisateurs' class='demande_nom_utilisateur' id='nombre_utilisateurs' min='1' max='40' placeholder='Nombre de participants' required></input>
+                            </br>
+                        <input type='submit' value='Valider' class='BoutonValidation' >
+                    </form>
+    </body>
