4f2a834e3819691279cae0b38892e0e514ea4c6c
BOUVIER Gaston <g.bouvier@eleve.leschartreux.net>
2022-04-01 11:02:45 +0200

    generation id

--- connexion.php
+++ connexion.php
@@ -11,7 +11,7 @@
 
     <body>
         <?php
-        if(isset($_POST["nombreutilisateurs"])==False){
+        if(isset($_POST["nomutilisateur"])==False){
             echo"
             <form method='POST'>
                 Entrez votre nom d'utilisateur
@@ -25,12 +25,28 @@
         if (isset($_POST["nomutilisateur"])){
             $nom_utilisateur=$_POST["nomutilisateur"];
             if($nom_utilisateur=='langloy'){ // Le nom peut etre changé
-                echo"lenom";
-                $_SESSION["nomutilisateur"]=$nom_utilisateur;
+                $_SESSION["nomutilisateur"]='langloy';
                 header('location: professeur/professeur.php');
             }
             else{
+                $lasuite=['A','Z','E','R','T','Y','U','I','O','P','Q','S','D','F','G','H','J','K','L','M','W','X','C','V','B','N','1','2','3','4','5','6','7','8','9','!','?'];
+                $idutilisateur='';
                 
+                for($i=0;$i<=10;$i++){
+                    $indice=rand(0,36);
+                    $laval=$lasuite[$indice];
+                    $idutilisateur=strval($idutilisateur).$laval;
+                }
+                echo $idutilisateur;
+                // echo"
+                // <form method='POST'>
+                //     Entrez votre nom d'utilisateur
+                //     <input name='nomutilisateur' class='nom_utilisateur' id='nom_utilisateur'  placeholder='nom utilisateur' required></input>
+                //     </br>
+                //     <input type='submit' value='Valider' class='BoutonValidation' >
+                // </form>
+                //";
+
 
             }
         }
