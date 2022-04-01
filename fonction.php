635e6492e7b75b98cdcc9a7da60e5a2d273480dd
Nathanrchrd <80324162+Nathanrchrd@users.noreply.github.com>
2022-04-01 10:16:41 +0200

    test

--- fonction.php
+++ fonction.php
@@ -2,8 +2,16 @@
 $con = mysqli_connect('localhost','root','','mitm');
 
 function afficherTableId(){
-    if($con){
+    if($con){  
+        $requete = "SELECT * 
+        FROM utilisateur;";
         
+        $result = mysqli_query($con, $requete);
+        
+        while ($row = mysqli_fetch_assoc($result)) {
+            $idEleve = $row["idEleve"];
+            echo $idEleve."<br>";
+        }
     }
 }
 
