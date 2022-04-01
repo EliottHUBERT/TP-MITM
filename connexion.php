f1d88ac31718842dc33dee6afd9db3b15d669916
BOUVIER Gaston <g.bouvier@eleve.leschartreux.net>
2022-04-01 11:46:58 +0200

    connexion

--- connexion.php
+++ connexion.php
@@ -4,6 +4,7 @@
         <title></title>
         <link rel="stylesheet" href="Styles/styles.css" type="text/css" />
         <?php
+           include('Fonction/connexion_sql.php');
            session_set_cookie_params(1);
            session_start();
         ?>
@@ -31,22 +32,40 @@
             else{
                 $lasuite=['A','Z','E','R','T','Y','U','I','O','P','Q','S','D','F','G','H','J','K','L','M','W','X','C','V','B','N','1','2','3','4','5','6','7','8','9','!','?'];
                 $idutilisateur='';
-                
+                $_SESSION["nomutilisateur"]=$nom_utilisateur;
+
                 for($i=0;$i<=10;$i++){
                     $indice=rand(0,36);
                     $laval=$lasuite[$indice];
                     $idutilisateur=strval($idutilisateur).$laval;
                 }
-                echo $idutilisateur;
-                // echo"
-                // <form method='POST'>
-                //     Entrez votre nom d'utilisateur
-                //     <input name='nomutilisateur' class='nom_utilisateur' id='nom_utilisateur'  placeholder='nom utilisateur' required></input>
-                //     </br>
-                //     <input type='submit' value='Valider' class='BoutonValidation' >
-                // </form>
-                //";
+                
+                
+                if($connexion){
+                    $requete = "INSERT INTO `utilisateur` (`id`, `idEleve`, `login`, `message`) VALUES (null, '$idutilisateur', '$nom_utilisateur', '');";
+                    $larequete = mysqli_query($connexion,$requete);
+                    echo'insertion réussie</br>';
+                
+                    echo"
+                        <form method='POST'>
+                            Entrez le nom de votre binome
+                            <input name='nom_binome' class='nom_utilisateur'  placeholder='nom binome' required></input>
+                            </br>
+                            <input type='hidden' name='nomutilisateur'>
+                            <input type='submit' value='Valider' class='BoutonValidation' >
+                        </form>
+                        ";
 
+                    if (isset($_POST["nom_binome"])){
+                        $le_binome=$_POST["nom_binome"];
+                        echo $le_binome;
+                        $requete = "SELECT 'id' FROM `utilisateur` WHERE (`login`='$le_binome');";
+                        $larequete = mysqli_query($connexion,$requete);
+                        $resultatrequete=mysqli_fetch_array($larequete);
+                        $lid_du_binome=$resultatrequete['id'];
+                        echo $lid_du_binome;
+                    }
+                }
 
             }
         }
