1a9ca083bf7b4e1b9ce69f63c51554109d12c801
BOUVIER Gaston <g.bouvier@eleve.leschartreux.net>
2022-04-08 08:34:22 +0200

    autres changements

--- eleve/interfaceEleve.php
+++ eleve/interfaceEleve.php
@@ -1,5 +1,6 @@
 <?php
 include ('../fonction.php');
+
 ?>
 
 <!DOCTYPE html>
@@ -7,17 +8,21 @@ include ('../fonction.php');
     <head>
         <meta charset="UTF-8"> 
         <link href="../Styles/styles.css" rel="stylesheet" type="text/css">
-        <title> Briefing MITM </title>
+        <title> Interface Eleve </title>
     </head>
     <body>
-        <div class="console_requete">
-           <div class="requete">
-               <br>
-                <p>qsd</p>
-                <p>qsd</p>
-                <br>
-           </div>
-        </div>
+
+        <div class = "fieldset">
+            <fieldset>
+                <div class = "bouton">
+                Demander la table de relation des Etudiants/ID : <button onclick="afficherEtudiantId()"> AfficherID </button> <br>
+                Etablissement de la communication avec un ID : <button onclick="test()"> Click </button> <br>
+                Identification via login / pass auprès d’un ID : <button onclick="test()"> Click </button> <br>
+                Message secret auprès d’un ID : <button onclick="test()"> Click </button>
+                </div>
+            </fieldset>
+
+        
         <div class="console_reponse">
             <div class="reponse">
                 <br>
