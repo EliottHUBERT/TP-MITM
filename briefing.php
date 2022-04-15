bc11aadc65cab11fd1759866789aa1693e6c40fb
Eliott HUBERT <e.hubert@eleve.leschartreux.net>
2022-04-15 09:17:31 +0200

    cookie

--- briefing.php
+++ briefing.php
@@ -8,7 +8,7 @@ setcookie('id', $idrep)
 <html>
     <head>
         <meta charset="UTF-8">
-        <META HTTP-EQUIV="Refresh" CONTENT="1; URL=/TP-MITM/eleve/interfaceEleve.php"> 
+        <META HTTP-EQUIV="Refresh" CONTENT="; URL=/TP-MITM/eleve/interfaceEleve.php"> 
         <link href="styles/styles.css" rel="stylesheet" type="text/css">
         <title> Briefing MITM </title>
     </head>
@@ -21,7 +21,11 @@ setcookie('id', $idrep)
                 <p id="Titre">Phase de compréhension : </p> <p><ul><li> TP à mener impérativement en binôme.</li><li> Votre binôme ne peut-être sur le même îlot que vous.</li><li>Aucune communication entre les étudiants après la phase de compréhension.</li><li> Seulement 20 min sont dédié à la compréhension du TP. Après les phases normales commencent.</li><li> Toutes documentations, échanges, sources, demande d’aide du professeur, ordinateur portable ... est interdit durant les phases normales.</li><li> Chaque étudiant doit définir avec son binôme un login/pass qui sera accepté lors de leur communications.</li><li> Le TP fonctionne par phase. A chaque phase l’étudiants peut faire dans l’ordre qu’il veut: <ul> <li>Une action de communication : </li><li>Demander la table de relation des Etudiants/ID </li><li>Etablissement de la communication avec un ID </li><li>Identification via login / pass auprès d’un ID </li><li>Message secret auprès d’un ID </ul></li><li>Et une action malveillante : <ul><li>Demander à modifier un ID </li><li>Bloquer un ID sur une phase</li><li> Ecouter un ID </li></ul> <li>Toutes les actions sont traitées en FIFO (First in / First out). </li><li>Une fois toutes les actions passées, une nouvelle phase débute. </li></ul> </p>
                 <p id="Titre">Phase finale:</p><p><ul><li> Envoyer un message à votre enseignant avec :</li><ul><li>Votre message Secret </li><li>Le nom de votre binôme </li><li>Le message secret de votre binôme (si vous l’avez😈) </li><li>Le ou les messages secrets obtenus (si vous en avez😈) </p>
                 <br>
-            </div>  
+                
+            </div>
+            <form action="connexion.php">
+                <input type=submit value="continuer">
+            </form> 
         </div>
     </body>
 </html>
