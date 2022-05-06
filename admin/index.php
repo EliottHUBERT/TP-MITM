67f3a4c084c5e6b18aba9bf77f697ffa6417bc2c
adrienbrt <a.barrat@eleve.leschartreux.net>
2022-05-06 09:06:06 +0200

    admin_style1

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ require ('../Fonction/connexion_sql.php')
 <head>
     <meta charset="utf-8">
     <title>Page admin</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../Styles/admin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
